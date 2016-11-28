b92d05f03609972cf8d599f5d8979b2d9630224b
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-11-28 14:47:39 -0800

    Initialize Fn

--- src/app/views/quote_view.js
+++ src/app/views/quote_view.js
@@ -1,5 +1,11 @@
 // View for an individual quote.
 
 var QuoteView = Backbone.View.extend({
+  initalize: function(options) {
+    this.symbol = options.symbol;
+    this.price = options.price;
 
+    // Compile a template to be shared between the individual quotes
+    this.quoteTemplate = _.template($('#tmpl-quote-view').html());
+  }
 });
