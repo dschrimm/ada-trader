db73718c68e0b17e0c03f2acbc34480550790241
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-11-28 14:59:55 -0800

    Render fn and app quote data

--- src/app.js
+++ src/app.js
@@ -5,6 +5,19 @@ import $ from 'jquery';
 import _ from 'underscore';
 import ApplicationView from 'app/views/application_view';
 
+var quoteData = [
+  {
+    symbol: 'HUMOR',
+    price: 88.50
+  }, {
+    symbol: 'CLOTH',
+    price: 81.70
+  }, {
+    symbol: 'SUPER',
+    price: 83.10
+  }
+];
+
 const simulate = function(quote) {
   // Calculate a random price movement
   const maxChange = 1.00;
--- src/app/views/quote_view.js
+++ src/app/views/quote_view.js
@@ -7,5 +7,19 @@ var QuoteView = Backbone.View.extend({
 
     // Compile a template to be shared between the individual quotes
     this.quoteTemplate = _.template($('#tmpl-quote-view').html());
+
+  },
+
+  render: function() {
+    var html = this.quoteTemplate(
+      {
+        symbol: this.symbol,
+        price: this.price
+     });
+
+    this.$el.html(html);
+
+    // Enable chained calls
+    return this;
   }
 });
