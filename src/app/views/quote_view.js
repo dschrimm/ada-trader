35ea91fcb34ab51214e5cf2d6e159cf8abc55c9e
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-11-28 16:11:20 -0800

    Events property defines click handlers

--- src/app/views/quote_view.js
+++ src/app/views/quote_view.js
@@ -8,15 +8,27 @@ var QuoteView = Backbone.View.extend({
     this.price = options.quote.price;
 
     this.template = options.template;
-
   },
 
   render: function() {
     var html = this.template({symbol: this.symbol, price: this.price});
     this.$el.html(html);
-    
+
     // Enable chained calls
     return this;
+  },
+
+  events: {
+    'click .btn-sell': 'sellQuote',
+    'click .btn-buy': 'buyQuote'
+  },
+
+  sellQuote: function(event) {
+    console.log('sell button was pressed');
+  },
+
+  buyQuote: function(event) {
+    console.log('buy button was pressed');
   }
 });
 
